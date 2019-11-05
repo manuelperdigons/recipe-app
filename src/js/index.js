42b8009269c3769ab60700c8994304e6f2e49dc4
Manuel Perdigon <manuelperdigonsocas@gmail.com>
2019-11-05 12:12:30 +0100

    Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -26,12 +26,19 @@ const controlSearch = async () => {
         searchView.clearInput();
         searchView.clearResults();
         renderLoader(elements.searchRes);
+
+        try {
         // 4 Search for recipes
         await state.search.getResults();
 
         // 5 render results on UI
         clearLoader();
         searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with Search');
+            clearLoader();
+        }
+
     }
 }
 
@@ -54,7 +61,32 @@ elements.searchResPages.addEventListener('click', e => {
  * Recipe Controller
  */
 
+ const controlRecipe = () => {
+    // get ID from url    
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
 
- const r = new Recipe(46956);
- r.getRecipe();
- console.log(r);
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+        // Get Recipe data
+        await state.recipe.getRecipe();
+        // Calculate time and servings
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+        // Render Recipe
+        console.log(state.recipe); 
+        } catch (err) {
+            alert('Error processing Recipe');
+        }
+
+    }
+ };
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,7 +9,7 @@ export default class Search {
     async getResults() {
         const key = `e37fb42365c76478f64f9d8206f19a94`;
         try {
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
             this.result = res.data.recipes;
             // console.log(this.result);
         } catch (error) {
